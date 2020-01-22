b69919da839cc96aaefbe9a25de35ae94fd80613
Alexey Kozlov <mail@reg65.com>
2020-01-23 07:05:05 +1100

    1

--- djshop/settings.py
+++ djshop/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'mptt',
+    'mptt',
+    'photologue',
+    'sortedm2m',
 ]
 
 MIDDLEWARE = [
